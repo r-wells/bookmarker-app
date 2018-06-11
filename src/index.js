324496a263a818748e33cc5f6cdaf597c82f8d1e
Ryan <r.l.wells@outlook.com>
2018-06-10 19:52:00 -0700

    added js functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
-import App from "./App";
-import style from "./main.css";
+//Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
+//Save bookmark
+function saveBookmark(e){
+    //Get form values
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    }
+
+    if(localStorage.getItem('bookmarks') === null) {
+        var bookmarks = [];
+
+        //Add to array
+        bookmarks.push(bookmark);
+
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        // fetch from localStorage
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        // Add bookmark to array
+        bookmarks.push(bookmark);
+        //Re-set back to localStorage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    //Prevents form from submitting automatically
+    e.preventDefault();
+}
+
+function fetchBookmarks() {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+    //Get output id
+    var bookmarksResults = document.getElementById('#bookmarksResults');
+
+    bookmarksResults.innerHTML = '';
+    for(var i = 0; i < bookmarks.length; i++) {
+        console.log(i);
+        // var name = bookmarks[i].name;
+        // var url = bookmarks[i].url;
+
+        // bookmarksResults.innerHTML += name;
+    }
+}
